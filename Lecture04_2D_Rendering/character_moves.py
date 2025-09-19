f66b7d189c83e06e04b85bfb35b7871748a6b4f3
sanghyeok1112 <tjtjtkdgur12@naver.com>
2025-09-19 21:15:11 +0900

    top lline 완성

--- Lecture04_2D_Rendering/character_moves.py
+++ Lecture04_2D_Rendering/character_moves.py
@@ -5,8 +5,37 @@ open_canvas()
 boy = load_image('character.png')
 grass = load_image('grass.png')
 
+def draw_boy(x: float, y: float):
+    clear_canvas_now()
+    boy.draw_now(x, y)
+    delay(0.01)
+
+def move_top():
+    print('Move Top')
+    for x in range(0, 800, 5):
+        draw_boy(x, 550)
+    pass
+
+def move_right():
+    print("Move Right")
+    pass
+
+def move_bottom():
+    print("Move Bottom")
+    pass
+
+def move_left():
+    print("Move Left")
+    pass
+
+
 def move_rectangle():
+
     print("Move Rectangle")
+    move_top()
+    move_right()
+    move_bottom()
+    move_left()
     pass
 
 def move_circle():
@@ -15,15 +44,11 @@ def move_circle():
     for deg in range(0, 360):
         x = r * math.cos(math.radians(deg)) + 400
         y = r * math.sin(math.radians(deg)) + 300
-
-        clear_canvas_now()
-        boy.draw_now(x, y)
-        delay(0.01)
-
+        draw_boy(x, y)
     pass
 
 while True:
-    move_circle()
+    # move_circle()
     move_rectangle()
     break
     pass
