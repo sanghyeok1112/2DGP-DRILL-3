5bc7086e13e6873f449f2f8ec2e15c94f23893bc
sanghyeok1112 <tjtjtkdgur12@naver.com>
2025-09-19 20:46:09 +0900

    캐릭터 출력 구현 완료

--- Lecture04_2D_Rendering/character_moves.py
+++ Lecture04_2D_Rendering/character_moves.py
@@ -1,20 +1,23 @@
 from pico2d import *
 
 open_canvas()
+boy = load_image('character.png')
+grass = load_image('grass.png')
 
 def move_rectangle():
     print("Move Rectangle")
     pass
 
-
 def move_circle():
     print("Move Circle")
+    clear_canvas_now()
+    boy_draw_now(400, 300)
     pass
 
-
 while True:
-    move_rectangle()
     move_circle()
+    move_rectangle()
+    break
     pass
 
 close_canvas()
