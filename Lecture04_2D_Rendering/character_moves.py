05dd510ed86ddf2a07b518ee84ad03f3a149c155
sanghyeok1112 <tjtjtkdgur12@naver.com>
2025-09-19 20:34:17 +0900

    사각운동, 원운동 함수 완성

--- Lecture04_2D_Rendering/character_moves.py
+++ Lecture04_2D_Rendering/character_moves.py
@@ -2,7 +2,17 @@ from pico2d import *
 
 open_canvas()
 
+def move_rectangle():
+    pass
+
+
+def move_circle():
+    pass
+
+
 while True:
+    move_rectangle()
+    move_circle()
     pass
 
 close_canvas()
